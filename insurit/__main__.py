ccdfe29caa1b90292ea37d06e865e1ba41e4ffc0
namansnghl <48488518+namansnghl@users.noreply.github.com>
2023-11-15 23:31:23 -0500

    added db connection script

--- insurit/__main__.py
+++ insurit/__main__.py
@@ -1,6 +1,7 @@
-import argparse, getpass
+import argparse
 from .backend import authenticate as auth
 import insurit.commands as commands
+from .backend.db_manager import Connection
 
 
 def main():
@@ -16,8 +17,8 @@ def main():
 
     # Subcommand: start service
     service_parser = subparsers.add_parser(start_service, help="Start insurit application by connecting to remote DB")
-    service_parser.add_argument('--hostname', help="Host of the Insurit service", default="value")
-    service_parser.add_argument('--port', help="Port number to connect with service", default="value")
+    service_parser.add_argument('--hostname', help="Host of the Insurit service")
+    service_parser.add_argument('--port', help="Port number to connect with service")
     service_parser.add_argument('--username', help="Username to access the service")
 
     # Subcommand: login
@@ -33,11 +34,10 @@ def main():
         print("Please provide a subcommand. Use 'insureit --help' for more information.")
     # if asked to connect db
     elif args.command == start_service:
-        if not args.username:
-            u = input("Username: ")
-        pswd = getpass.getpass("Password")
-        print("connect with DB here")
-    # if asked to login
+        conn = Connection(hostname=args.hostname, port=args.port, username=args.username)
+        conn.fetch_creds()
+        conn.connect()
+    # if asked to log in
     elif args.command == login_command:
         if auth.login(args.username, args.password):
             print("Authentication successful.\nWelcome ", args.username)
