fd123af67e7157afbcdb8d3a7d37397fd9f07386
namansnghl <48488518+namansnghl@users.noreply.github.com>
2023-11-18 17:00:57 -0500

    login complete

--- insurit/app.py
+++ insurit/app.py
@@ -10,6 +10,9 @@ def run(app: str, conn):
     global connection
     connection = conn
     login_command = 'login'
+    login_agent = 'agent'
+    login_root = 'root'
+    levels = {login_command: 2, login_agent: 1, login_root: 0}
 
     print("Application Starting\n WELCOME TO INSURIT - YOUR TRUSTED INSURANCE PROVIDER")
     loginparser = argparse.ArgumentParser()
@@ -19,9 +22,21 @@ def run(app: str, conn):
     login_parser = subparsers.add_parser(login_command, help="Login to Insureit")
     login_parser.add_argument("-u", "--username", help="Your Insureit login username", required=True)
     login_parser.add_argument("-p", "--password", help="Your Insureit login password", required=True)
+    # Subcommand: agent
+    login_parser = subparsers.add_parser(login_agent, help="Agent login to Insureit")
+    login_parser.add_argument("-u", "--username", help="Insureit login username", required=True)
+    login_parser.add_argument("-p", "--password", help="Insureit login password", required=True)
+    # Subcommand: root
+    login_parser = subparsers.add_parser(login_root, help="Administrator login to Insureit")
+    login_parser.add_argument("-u", "--username", help="Insureit login username", required=True)
+    login_parser.add_argument("-p", "--password", help="Insureit login password", required=True)
 
     while True:
         user_input = input(f"\n{app}> ")
+        if user_input.strip().lower() in ['exit', 'cancel', 'disconnect']:
+            Connection.disconnect(conn)
+            print("Connection closed, Exiting...")
+            break
 
         parts = shlex.split(user_input)
         # handle package name if parsed (we don't need package name anymore)
@@ -36,13 +51,12 @@ def run(app: str, conn):
         except:
             unknown_command(loginparser)
         else:
-            if args.command == login_command:
-                if auth.login(args.username, args.password):
-                    print("Authentication successful.\nWelcome ", args.username)
-                    if loggedin(app, args.username):
-                        user_input = 'exit'
-                else:
-                    print("Authentication failed. Incorrect Username/Password.")
+            if auth.login(conn.cursor(), args.username, args.password, levels[args.command]):
+                print("Authentication successful.\nWelcome ", args.username)
+                if loggedin(app, args.username):
+                    user_input = 'exit'
+            else:
+                print("Authentication failed. Incorrect Username/Password.")
 
         if user_input.strip().lower() in ['exit', 'cancel', 'disconnect']:
             Connection.disconnect(conn)
