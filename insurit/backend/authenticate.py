7c7e14d846c43760bb928edf86087028d20d9cfe
Naman Singhal <48488518+namansnghl@users.noreply.github.com>
2023-11-18 17:03:07 -0500

    Login Authorization part Completed (#11)
    * Added permission level to secrets
    
    * login complete

--- insurit/backend/authenticate.py
+++ insurit/backend/authenticate.py
@@ -1,4 +1,9 @@
 # backend
-def login(username: str, password: str) -> bool:
-	# Return True if authentication is successful, False otherwise
-	return username == "naman" and password == "naman"
+def login(cursor, username: str, password: str, login_level: int) -> int:
+	SQL = "SELECT AUTH(%s, %s, %s);"
+	params = (username, password, login_level)
+	cursor.execute(SQL, params)
+	response = cursor.fetchone()
+	cursor.close()
+	return response[0]
+
