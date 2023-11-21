f94b7d655d6c41b5a1e59694f31d675ff13cd64d
Karan Badlani <karanbadlani1@gmail.com>
2023-11-20 19:42:32 -0500

    Update

--- insurit/backend/sql_scripts/Functions/calPremium.sql
+++ insurit/backend/sql_scripts/Functions/calPremium.sql
@@ -1,16 +1,7 @@
-DELIMITER //
-
-CREATE FUNCTION CalculatePremium_Modified(customer_id INT, sum_assured INT, tenure INT, policy_type VARCHAR(255))
-RETURNS DECIMAL(10, 2)
-DETERMINISTIC
+CREATE DEFINER=`root`@`localhost` FUNCTION `CalculatePremium`(policy_id INT, age INT, sum_assured INT, tenure INT, policy_type VARCHAR(255)) RETURNS decimal(10,2)
+    DETERMINISTIC
 BEGIN
     DECLARE base_premium DECIMAL(10, 2);
-    DECLARE customer_age INT;
-
-    -- Retrieve age from Customers table
-    SELECT Age INTO customer_age
-    FROM Customers
-    WHERE Customer_id = customer_id;
 
     -- Assume some logic for calculating a base premium based on policy type, sum assured, and tenure
     -- You may need to customize this logic according to your business requirements
@@ -24,12 +15,10 @@ BEGIN
     -- Apply age-based adjustment to the premium
     SET base_premium = 
         CASE
-            WHEN customer_age >= 18 AND customer_age <= 25 THEN base_premium * 1.2 -- 20% increase for age 18-25
-            WHEN customer_age > 65 THEN base_premium * 1.5 -- 50% increase for age over 65
+            WHEN age >= 18 AND age <= 25 THEN base_premium * 1.2 -- 20% increase for age 18-25
+            WHEN age > 65 THEN base_premium * 1.5 -- 50% increase for age over 65
             ELSE base_premium
         END;
 
     RETURN base_premium;
-END //
-
-DELIMITER ;
+END
