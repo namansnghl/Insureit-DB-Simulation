5bc1446d6f2ea0e0f8bbf99e71e2e8594dad0061
Isha Singh <ishas2505@gmail.com>
2023-12-01 14:27:15 -0500

    All changes are done

--- insurit/backend/insertdata.py
+++ insurit/backend/insertdata.py
@@ -1,20 +1,5 @@
-import mysql.connector
 
-def read_database_config(file_path="requirements.txt"):
-    with open(file_path, 'r') as file:
-        config = {line.split('=')[0]: line.split('=')[1].strip() for line in file}
-    return config
-
-def insertdata():
-    config = read_database_config()
-
-    connection = mysql.connector.connect(
-        host=config["host"],
-        user=config["user"],
-        password=config["password"],
-        database=config["database"]
-    )
-
+def insertdata(connection):
     cursor = connection.cursor()
 
     try:
@@ -25,6 +10,5 @@ def insertdata():
         print(f"Error: {e}")
     finally:
         cursor.close()
-        connection.close()
 
-insertdata()
+#insertdata()
--- insurit/backend/user_management.py
+++ insurit/backend/user_management.py
@@ -1,9 +1,8 @@
 # create user() --This function takes the input from the user (Customer or Agent) and then calls the respective function.
-def userType():
+def userType(n):
     print('-----Please choose your type-----')
     print('Type 1 for Agent')
     print('Type 2 for Customer')
-    n = int(input('Your type?'))
     print('\n')
     if n==1:
         createCustomer()
@@ -15,26 +14,26 @@ def userType():
 # create Cust() -- This function is used to create a new customer.
 def createCustomer(connection):
     cursor = connection.cursor()
-    Name = input("Enter your name:")
-    Phone = input("Enter your phone (without country code):")
-    Email = input("Enter your email:")
-    Address = input("Enter your Address:")
-    Driving_license = input("Please type Yes if you have a Driving license, otherwise No.")
-    Age = int(input("Enter your age:"))
+    Name = input("Enter customer name:")
+    Phone = input("Enter customer phone (without country code):")
+    Email = input("Enter customer email:")
+    Address = input("Enter customer Address:")
+    Driving_license = input("Please type Yes if customer has a Driving license, otherwise No.")
+    Age = int(input("Enter customer age:"))
     query = """INSERT INTO 
     Customer (Name, Phone, Email, Address,Driving_license,Age) values (%s,%s,%s,%s,%s,%s)"""
     vals = (Name,Phone,Email,Address,Driving_license,Age)
     cursor.execute(query,vals)
     connection.commit()
-    print('---Thanks for registering. Your registration is successful.---')
+    print('---Thanks for registering. Customer registration is successful.---')
 
 # create Agent() This function is used to create a new agent.
 def createAgent(connection):
     cursor = connection.cursor()
-    Name = input("Enter your name:")
-    Email = input("Enter your email:")
-    Phone = input("Enter your phone (without country code):")
-    Salary = int(input("Enter your Salary:"))
+    Name = input("Enter agent name:")
+    Email = input("Enter agent email:")
+    Phone = input("Enter agent phone (without country code):")
+    Salary = int(input("Enter agent Salary:"))
     query = """INSERT INTO 
     Agents (Name, Email, Phone, Salary) values (%s,%s,%s,%s)"""
     vals = (Name, Email, Phone, Salary)
