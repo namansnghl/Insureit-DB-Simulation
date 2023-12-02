45b77e998950f9d1a8173c1a4d1f25d31abff6fd
Karan Badlani <karanbadlani1@gmail.com>
2023-12-02 11:56:35 -0500

    forms.py and utils.py done

--- insurit/backend/forms.py
+++ insurit/backend/forms.py
@@ -1,2 +1,75 @@
 # create claims
 # create policy KARAN
+
+#The function below would take connection as a parameter and some other info 
+#from user and would call the addNewPolicy SP to create a new policy under either HomePolicy_detail or AutoPolicy_detail
+#Manager/admin input for the params below
+def add_new_policy(connection):
+    try:
+        cursor = connection.cursor()
+
+        print("Please fill out below details to Add a new Policy to the Insurit System.\n")
+        policy_name = input("Enter the name of the Policy: ")
+        policy_type = input("Enter Policy Name: HomePolicy or AutoPolicy: ")
+        description = input("Enter the Description: ")
+        policy_active_flag = "Y"
+        sum_assured = int(input("Enter the sum assured for this policy: "))
+        tenure = int(input("What's the Tenure: "))
+
+        if policy_name == "HomePolicy": policy_table = "HomePolicy_detail"
+        else:  policy_table = "AutoPolicy_detail" 
+
+
+        # Set parameters for the stored procedure
+        params = (policy_name, policy_type, description, policy_active_flag, sum_assured, tenure, policy_table)
+        #take policy_table from policy_type variable.
+
+        # Call the stored procedure
+        cursor.callproc('AddNewPolicy', params)
+
+        # Commit the changes
+        connection.commit()
+
+        print("New policy added successfully!")
+
+    except mysql.connector.Error as err:
+        # Handle MySQL errors here
+        print(f"MySQL Error: {err}")
+
+    finally:
+        # Close the cursor
+        if cursor:
+            cursor.close()
+
+
+
+#Please import the below library in the main file 
+#import datetime from date
+
+def create_new_claim(connection):
+    try:
+        # Get input from the user
+        claim_amount = int(input("Enter claim amount: "))
+        holder_id = int(input("Enter holder ID: "))
+
+        cursor = connection.cursor()
+
+        # Call the stored procedure
+        #Note that the claim date for newly created row would be for currrent date only
+        cursor.callproc('CreateNewClaim', (claim_amount, date.today(), holder_id))
+
+        # Commit the changes
+        connection.commit()
+
+        print("New claim created successfully!")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        # Close the cursor
+        cursor.close()
+
+# # Example usage:
+# # Assuming you have a MySQL connection named 'conn'
+# create_new_claim(conn)
--- insurit/backend/utils.py
+++ insurit/backend/utils.py
@@ -1,2 +1,72 @@
 # calc_premium() SP KARAN
 # claim approvals()
+#Not importing any library as it will be directly done in main.py
+
+
+#Below function would take parameters such as policy_id, age, sum_assured, tenure and policy_type apart from connection
+#for the below params, take input directly when the agent is calling this function
+def calculate_premium(connection):
+    try:
+        cursor = connection.cursor()
+        print("Fill below to Calculate an Estimate of the Premium\n")
+        policy_id = input("Enter Policy ID: ")
+        age = int(input("Enter age of the customer: "))
+        sum_assured = int(input("Enter total sum assured for the following policy: "))
+        tenure = int(input("Enter the tenure period: "))
+        policy_type = input("Enter Policy type: HomePolicy or AutoPolicy: ")
+
+        # Calling the MySQL stored function
+        cursor.callproc('CalculatePremium', (policy_id, age, sum_assured, tenure, policy_type))
+
+        # Fetching the result from the stored function
+        result = cursor.fetchone()
+
+        # Ensure the result is not None
+        if result is not None:
+            # The premium value is the first element in the result tuple
+            premium_value = result[0]
+            print(premium_value)
+            return premium_value
+        else:
+            # Handle the case where the result is None
+            print("Error: Unable to retrieve premium value.")
+            return None
+
+    except mysql.connector.Error as err:
+        # Handle MySQL errors here
+        print(f"MySQL Error: {err}")
+        return None
+
+    finally:
+        # Close the cursor
+        if cursor:
+            cursor.close()
+
+
+#Claim_approvals would return a dataframe with all the approvals only 
+def get_approved_claims(connection):
+    try:
+        cursor = connection.cursor(dictionary=True)
+
+        # SQL query to select all data for approved claims
+        query = "SELECT * FROM claim_table WHERE claim_status = 'A'"
+
+        # Execute the query
+        cursor.execute(query)
+
+        # Fetch all rows
+        approved_claims = cursor.fetchall()
+
+        return approved_claims
+
+    except mysql.connector.Error as err:
+        # Handle MySQL errors here
+        print(f"MySQL Error: {err}")
+        return None
+
+    finally:
+        # Close the cursor
+        if cursor:
+            cursor.close()
+
+
