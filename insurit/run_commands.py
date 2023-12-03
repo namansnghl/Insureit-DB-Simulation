551cf30abb928948df8fa049e675e02adfc9707e
namansnghl <48488518+namansnghl@users.noreply.github.com>
2023-12-03 18:47:26 -0500

    Agent commands integrated

--- insurit/run_commands.py
+++ insurit/run_commands.py
@@ -1,4 +1,8 @@
 from .backend.user_management import userType
+from .backend.views import *
+from .backend.utils import *
+from datetime import datetime, timedelta
+from .backend.finance import *
 
 
 def run_command(args, **kwargs):
@@ -20,6 +24,14 @@ def run_agent(args, **kwargs):
     if args.agent == 'client':
         if args.new:
             userType(kwargs['conn'], kwargs['access_lvl'])
+        if args.show:
+            showCustomers(kwargs['conn'], kwargs['id'])
+    elif args.agent == 'policy':
+        if args.calc_premium:
+            calculate_premium(kwargs['conn'])
+        if args.at_risk:
+            check_due_dates(kwargs['conn'])
+
 
 def run_admin(args, **kwargs):
     ...
