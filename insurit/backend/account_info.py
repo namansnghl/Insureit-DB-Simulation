d9670aaa791252a466f653e860345925c67157db
namansnghl <48488518+namansnghl@users.noreply.github.com>
2023-12-03 19:31:22 -0500

    corrections

--- insurit/backend/account_info.py
+++ insurit/backend/account_info.py
@@ -3,65 +3,56 @@
 
 import os
 
-#function to update the email of the customer
+
+# function to update the email of the customer
 def updateEmail(connection, customer_id):
     cursor = connection.cursor()
-    print("Hi! Set up your new email")
     email = input('Enter your new Email:')
     query = "UPDATE customer SET email = %s WHERE Customer_id = %s"
-    cursor.execute(query,(email,customer_id))
+    cursor.execute(query, (email, customer_id))
     connection.commit()
     print('Email has been updated successfully!')
 
-#function to update the phone number of the customer
+
+# function to update the phone number of the customer
 def updatePhone(connection, customer_id):
     cursor = connection.cursor()
-    print("Hi! Set up your new phone number")
     phone = input('Enter your new Phone Number:')
     query = "UPDATE customer SET Phone = %s WHERE Customer_id = %s"
-    cursor.execute(query,(phone,customer_id))
+    cursor.execute(query, (phone, customer_id))
     connection.commit()
     print('Phone Number has been updated successfully!')
 
-#function to update the addresss of the customer
+
+# function to update the addresss of the customer
 def updateAddress(connection, customer_id):
     cursor = connection.cursor()
-    print("Hi! Set your new home address")
     address = input('Enter your new Address:')
     query = "UPDATE customer SET Address = %s WHERE Customer_id = %s"
-    cursor.execute(query,(address,customer_id))
+    cursor.execute(query, (address, customer_id))
     connection.commit()
     print('Address has been updated successfully!')
 
 
 def menu():
-    print('WELCOME TO INSURIT!')
-    print('-----------------------------')
-    print('Menu')
+    print('WARNING: Changing account settings')
     print('1. Update Email')
     print('2. Update Phone')
     print('3. Update Address')
     print('4. Cancel')
-    print('-----------------------------')
+    return int(input('Your choice: '))
 
-def runInsurit(connection, customer_id, option=None):
-    menu()
-    #option = int(input('Please choose an option'))
-    print('\n')
-    if option==1:
-        print('\n')
-        updateEmail(customer_id)
-        print('\n')
-    if option==2:
-        print('\n')
-        updatePhone(customer_id)
-        print('\n')
-    if option==3:
-        print('\n')
-        updateAddress(customer_id)
-        print('\n')
-    if option==4:
+
+def chsettings(connection, customer_id, option=None):
+    if not option:
+        option = menu()
+    if option == 1:
+        updateEmail(connection, customer_id)
+    elif option == 2:
+        updatePhone(connection, customer_id)
+    elif option == 3:
+        updateAddress(connection, customer_id)
+    elif option == 4:
         return 0
     else:
-        os.system('cls')
-        runInsurit()
+        chsettings(connection, customer_id, option)
